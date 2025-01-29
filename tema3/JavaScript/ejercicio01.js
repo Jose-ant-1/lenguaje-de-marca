0a653c7453e8df9bab01a2669ac67663d94f5a67
Jose-ant-1 <jgonsol1510@g.educaand.es>
2025-01-29 13:53:03 +0100

    e

---
+++ tema3/JavaScript/ejercicio01.js
@@ -0,0 +1,53 @@
+const nombre = "Éxarion";
+let nivel = 5;
+let vida = 100;
+const armaPrincipal = "Espada del Destino";
+let armaSecundaria = "Arco simple";
+let armadura = true;
+console.log(`Estado del héroe:`);
+console.log(`Nombre: ${nombre}`);
+console.log(`Nivel: ${nivel}`);
+console.log(`Puntos de vida: ${vida}`);
+console.log(`Arma principal: ${armaPrincipal}`);
+console.log(`Arma secundaria: ${armaSecundaria}`);
+console.log(`Tiene armadura: ${armadura}`);
+
+nivel++;
+vida -= 30;
+console.log(`Estado del héroe:`);
+console.log(`Nombre: ${nombre}`);
+console.log(`Nivel: ${nivel}`);
+console.log(`Puntos de vida: ${vida}`);
+console.log(`Arma principal: ${armaPrincipal}`);
+console.log(`Arma secundaria: ${armaSecundaria}`);
+console.log(`Tiene armadura: ${armadura}`);
+
+let nivelCadena = nivel.toString();
+let puntosFloat = parseFloat(vida);
+
+console.log(`Nivel: ${nivelCadena}`);
+console.log(`Puntos de vida: ${puntosFloat}`);
+
+let danyoBase = 50;
+let multCritico = 2;
+let probCritico = 0.5; 
+let EsCritico = probCritico > Math.random();
+
+let variable = esCritico ? danyoBase * multCritico : danyoBase;
+console.log(`El heroe inflige ${danyoBase} puntos de daño porque es critico tiene valor` + esCritico);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
