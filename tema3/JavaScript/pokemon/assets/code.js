aad70ed1b9ae58e8c4c11e432ff06dafeeea5bb0
Jose-ant-1 <jgonsol1510@g.educaand.es>
2025-03-05 13:49:31 +0100

    kmhjfñ

---
+++ tema3/JavaScript/pokemon/assets/code.js
@@ -0,0 +1,46 @@
+const boton = document.querySelector("#buscar");
+
+boton.addEventListener("click", buscarPokemon);
+
+async function buscarPokemon(){
+
+    const nombreABuscar = document.querySelector("#nombreABuscar").value.toLowerCase;
+    const url = `https://pokeapi.co/api/v2/pokemon/${nombreABuscar}`;
+
+    const nombre = document.querySelector("#nombre");
+    const imagen = document.querySelector("#imagen");
+    const altura = document.querySelector("#altura");
+    const tipo = document.querySelector("#tipo");
+
+    try {
+        const response = await fetch(url);
+
+        const data = await response.json();
+
+        nombre.innerHTML= `<h3>${data.name}</h3>`;
+        imagen.src=data.sprites.fron_default;
+        altura.innerHTML=data.height;
+        data.types.forEach(Element =>{
+            tipo.innerHTML=tipo.innerHTML+`<li>${Element.type.name}</li>`;
+        });
+
+    } catch (error) {
+        imagen.src="";
+        tipo.innerHTML=``;
+        altura.innerHTML=``;
+        nombre.innerHTML=`<p> No se pudo encontrar el pokemon </p>`;
+        nombre.error("Error al obtener los datos", error);
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
